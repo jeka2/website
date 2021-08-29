a5afe9a246fc60d92c2bb250f4286bb8174daf3c
Yevgeniy Ivanov <anigeka@gmail.com>
2021-08-29 18:26:51 -0400

    add basic styling and animation to header options

--- src/components/header/header.compontent.jsx
+++ src/components/header/header.compontent.jsx
@@ -2,23 +2,32 @@ import React from 'react';
 import { Link, withRouter } from 'react-router-dom';
 import './header.styles.scss';
 
-const Header = () => (
-    <div className="header">
-        <Link className="logo-container" to="/">
-            Home
-        </Link>
-        <div className="options">
-            <Link className="option" to="/blogs">
-                Blogs
+class Header extends React.Component {
+
+    toggleStylizedLink(e, type) {
+        type === 'enter' ? e.target.classList.add('hovered') : e.target.classList.remove('hovered');
+    }
+
+    render() {
+        return(
+        <div className="header">
+            <Link className="logo" to="/">
+                Home
             </Link>
-            <Link className="option" to="/projects">
-                Projects
-            </Link>
-            <Link className="option" to="/about">
-                About Me
-            </Link>
+            <div className="options">
+                <Link className="option" to="/blogs" onMouseEnter={(e) => this.toggleStylizedLink(e, 'enter')} onMouseLeave={(e) => this.toggleStylizedLink(e, 'leave')}>
+                    Blogs
+                </Link>
+                <Link className="option" to="/projects">
+                    Projects
+                </Link>
+                <Link className="option" to="/about">
+                    About Me
+                </Link>
+            </div>
         </div>
-    </div>
-)
+        )
+    }
+}
 
 export default Header;
